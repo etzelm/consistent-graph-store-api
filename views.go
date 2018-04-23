a1e349d22d9e9195cab9df89352fda50b4886898
Mitchell Etzel <metzel@ucsc.edu>
2018-04-22 22:09:02 -0700

    Transferred Over Some Causal Functionality

--- views.go
+++ views.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"net/http"
+	"sort"
 
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
@@ -37,4 +40,59 @@ func CheckPut(c *gin.Context) {
 
 func LandingPage(c *gin.Context) {
 	c.String(http.StatusOK, "Hello!")
+}
+
+func stringifyCausal(m map[string]int64) string {
+	b := new(bytes.Buffer)
+	ip_ports := make([]string, 0, len(m))
+	for ip_port := range m {
+		ip_ports = append(ip_ports, ip_port)
+	}
+	sort.Strings(ip_ports)
+	for _, ip_port := range ip_ports {
+		fmt.Fprintf(b, "%s.", fmt.Sprintf("%d", m[ip_port]))
+	}
+	b = bytes.NewBuffer(bytes.Trim(b.Bytes(), "."))
+	return b.String()
+}
+
+//0 == greater && 1 == lesser && 2 == concurrent
+func CompareCausal(c1 map[string]int64, c2 map[string]int64) int {
+	less_seen := false
+	great_seen := false
+	if c1 == nil && c2 != nil {
+		return 1
+	} else if c1 != nil && c2 == nil {
+		return 0
+	}
+	ip_ports := make([]string, 0, len(c1))
+	for ip_port := range c1 {
+		ip_ports = append(ip_ports, ip_port)
+	}
+	for _, ip_port := range ip_ports {
+		if c1[ip_port] < c2[ip_port] {
+			less_seen = true
+		} else if c1[ip_port] > c2[ip_port] {
+			great_seen = true
+		}
+	}
+	if less_seen && !great_seen {
+		return 1
+	} else if !less_seen && great_seen {
+		return 0
+	}
+	return 2
+}
+
+func UpdateCausal(c1 map[string]int64, c2 map[string]int64) map[string]int64 {
+	ip_ports := make([]string, 0, len(c1))
+	for ip_port := range c1 {
+		ip_ports = append(ip_ports, ip_port)
+	}
+	for _, ip_port := range ip_ports {
+		if c1[ip_port] < c2[ip_port] {
+			c1[ip_port] = c2[ip_port]
+		}
+	}
+	return c1
 }
