1ff89bc64ca420defe26ea17be8a2b2c1aec5eb5
Mitchell Etzel <metzel@ucsc.edu>
2018-04-22 22:13:36 -0700

    Transferred Over Some Global Variables

--- main.go
+++ main.go
@@ -14,6 +14,33 @@ import (
 // IpPort is this computers IP address
 var IpPort string
 
+// VIEW is a list of current nodes in the view.
+var VIEW [][]Node
+
+// View is used as a type alias for a slice of Nodes
+type View [][]Node
+
+// Self represents this current node
+var SELF Node
+
+// K holds the size that our partitions are supposed to be
+var R int
+
+// partition_it holds the place we want to put the next node we add
+var partition_it int
+
+// partition_id holds the id of the parition the Node belongs too
+var partition_id int
+
+// num_partitions holds the number of partitions we currently have
+var num_partitions int
+
+// num_nodes holds the number of partitions we currently have
+var num_nodes int
+
+// server_causal holds the latest causal_payload we have currently seen on this server
+var server_causal map[string]int64
+
 func main() {
 
 	// testing:
